1ebfdb96ecf21c9f5f39638e2c7e2555fe6ecd39
HFG43 <hernanguemes@hotmail.com>
2023-12-29 20:47:10 -0300

    Feat: add destroy people method and POST New Person view

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,7 +1,35 @@
 class PeopleController < ApplicationController
+  def new
+    @person = Person.new
+  end
+
+  def create
+    @person = Person.new(person_params)
+
+    if @person.save
+      render json: @person, status: :created
+    else
+      render json: @person.errors, status: :unprocessable_entity
+    end
+  end
+
   def index
     @person = filtered_people
     render json: @person.as_json(except: %i[created_at updated_at], include: { addresses: { only: %i[id calle numero ciudad] } })
+  end
+
+  def show
+    @person = Person.find_by(dni: params[:id])
+  end
+
+  def destroy
+    person = Person.find_by(dni: params[:id])
+
+    if person.destroy
+      render json: person
+    else
+      render json: { errors: person.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
