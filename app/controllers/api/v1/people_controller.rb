754e4b6059866d8fd45ddc5645079fced98dde17
HFG43 <hernanguemes@hotmail.com>
2023-12-29 12:12:28 -0300

    Feat: persona Params

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::PeopleController < ApplicationController
+  def create; end
+
+  private
+
+  def people_params
+    params.require(:person).permit(:dni, :nombre, :apellido, :edad, :foto)
+  end
 end
