fa65e4f0c16b377298495bbd6c6beaacd653850f
HFG43 <hernanguemes@hotmail.com>
2023-12-29 10:08:30 -0300

    Feat: generacion de Personas Migration

---
+++ db/migrate/20231229124200_create_persons.rb
@@ -0,0 +1,14 @@
+class CreatePersons < ActiveRecord::Migration[7.0]
+  def change
+    create_table :persons, :id => false do |t|
+      t.bigserial :dni, primary_key: true
+      t.string :nombre, null: false
+      t.string :apellido, null: false
+      t.integer :edad, null: false
+      t.string :foto, null: false
+
+
+      t.timestamps
+    end
+  end
+end
