bfa417efa2e0da670cb30a76950a027505b05b9b
HFG43 <hernanguemes@hotmail.com>
2023-12-29 10:50:08 -0300

    Feat: Agregue asociacion de persona y direccion, y validaciones de persona

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,8 @@
 class Person < ApplicationRecord
+  has_many :addresses, class_name: 'Address', foreign_key: 'dni', dependent: :destroy
+
+  validates :dni, numericality: { only_integer: true, greater_than: 0 }
+  validates :dni, length: { is: 8 }
+  validates :nombre, :apellido, :edad, :foto, presence: true
+  validates :edad, comparison: { greater_than: 0, less_than: 99 }, numericality: { only_integer: true }
 end
