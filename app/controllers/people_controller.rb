30b859aaebf1fad46b6eeafcbfd1bad4d97c8e51
HFG43 <hernanguemes@hotmail.com>
2023-12-29 19:51:01 -0300

    Feat: add filter by Nombre, Edad y DNI

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,18 @@
 class PeopleController < ApplicationController
   def index
-    @person = Person.all
+    @person = filtered_people
     render json: @person.as_json(except: %i[created_at updated_at], include: { addresses: { only: %i[id calle numero ciudad] } })
+  end
+
+  private
+
+  def filtered_people
+    people = Person.all
+
+    people = people.where(dni: params[:dni]) if params[:dni].present?
+    people = people.where(nombre: params[:nombre]) if params[:nombre].present?
+    people = people.where(edad: params[:edad]) if params[:edad].present?
+
+    people
   end
 end
