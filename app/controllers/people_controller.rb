f482c9bbe1c74933b36e9389716c1eb5cbe2d159
HFG43 <hernanguemes@hotmail.com>
2024-01-01 18:01:28 -0300

    Feat: include Update-Destroy Methods and Messages

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -7,28 +7,81 @@ class PeopleController < ApplicationController
     @person = Person.new(person_params)
 
     if @person.save
-      render json: @person, status: :created
+      render json: {
+        status: 'SUCCESS',
+        message: 'Persona Creada',
+        data: @person.as_json
+      }, status: :ok
     else
-      render json: @person.errors, status: :unprocessable_entity
+      render json: {
+        status: 'ERROR',
+        message: 'No se pudo crear la nueva persona',
+        data: @person.errors.full_messages
+      }, status: :unprocessable_entity
     end
   end
 
   def index
-    @person = filtered_people
-    render json: @person.as_json(except: %i[created_at updated_at], include: { addresses: { only: %i[id calle numero ciudad] } })
+    @people = filtered_people.order('created_at DESC')
+    render json: {
+      status: 'SUCCESS',
+      message: 'Listado de Personas',
+      data: @people.as_json(
+        except: %i[created_at updated_at],
+        include: {
+          addresses: { only: %i[id calle numero ciudad] }
+        }
+      )
+    }, status: :ok
   end
 
   def show
     @person = Person.find_by(dni: params[:id])
+    render json: {
+      status: 'SUCCESS',
+      message: 'Persona por DNI',
+      data: @person.as_json(
+        except: %i[created_at updated_at],
+        include: {
+          addresses: { only: %i[id calle numero ciudad] }
+        }
+      )
+    }, status: :ok
+  end
+
+  def update
+    @person = Person.find_by(dni: params[:id])
+
+    if @person.update(person_params)
+      render json: {
+        status: 'SUCCESS',
+        message: 'Persona Actualizada',
+        data: @person.as_json
+      }, status: :ok
+    else
+      render json: {
+        status: 'ERROR',
+        message: 'No se pudo actualizar la informacion de la persona',
+        data: @person.errors.full_messages
+      }, status: :unprocessable_entity
+    end
   end
 
   def destroy
     person = Person.find_by(dni: params[:id])
 
     if person.destroy
-      render json: person
+      render json: {
+        status: 'SUCCESS',
+        message: 'Persona Eliminada de la Base',
+        data: @person.as_json
+      }, status: :ok
     else
-      render json: { errors: person.errors.full_messages }, status: :unprocessable_entity
+      render json: {
+        status: 'ERROR',
+        message: 'No se pudo eliminar la persona',
+        data: @erson.errors.full_messages
+      }, status: :unprocessable_entity
     end
   end
 
@@ -37,7 +90,6 @@ class PeopleController < ApplicationController
   def filtered_people
     people = Person.all
 
-    people = people.where(dni: params[:dni]) if params[:dni].present?
     people = people.where(nombre: params[:nombre]) if params[:nombre].present?
     people = people.where(edad: params[:edad]) if params[:edad].present?
 
