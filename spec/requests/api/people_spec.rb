6b890549cf19f6df358ef09791c0fc9e9ea49f82
HFG43 <hernanguemes@hotmail.com>
2024-01-02 10:39:56 -0300

    Feat: add post People endpoint test

--- spec/requests/api/people_spec.rb
+++ spec/requests/api/people_spec.rb
@@ -1,4 +1,5 @@
 require 'swagger_helper'
+require 'faker'
 
 RSpec.describe 'api/people', type: :request do
   path '/api/v1/people' do
@@ -29,6 +30,31 @@ RSpec.describe 'api/people', type: :request do
 
         let(:id) do
           Person.create(dni: 28_311_542, nombre: 'Hernan', apellido: 'Guemes', edad: 44, foto: 'subitufoto.com').dni
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/people' do
+    post 'Creates a Person' do
+      tags 'Person'
+      consumes 'application/json'
+      parameter name: :person, in: :body, schema: {
+        type: :object,
+        properties: {
+          dni: { type: :bigserial },
+          nombre: { type: :string },
+          apellido: { type: :string },
+          edad: { type: :integer },
+          foto: { type: :string }
+        },
+        required: %w[dni nombre apellido edad foto]
+      }
+
+      response '200', 'Persona Creada' do
+        let(:person) do
+          Person.new(dni: Faker::Number.unique.number(digits: 8), nombre: 'Nombre Test', apellido: 'Apellido Test', edad: 40, foto: 'subitufoto.com')
         end
         run_test!
       end
